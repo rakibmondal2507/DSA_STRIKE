229d56e28fe0a2fdc8bc555d5f86932b0ec64c4f
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2026-01-14 18:35:37 +0530

    Update Allocate minimum number of pages.cpp

--- Binary_Search/Allocate minimum number of pages.cpp
+++ Binary_Search/Allocate minimum number of pages.cpp
@@ -1,1 +1,42 @@
+bool allocateBook(int maxPages, vector<int> &arr, int k){
+	int count = 1, pages = arr[0];
+	for (int i=1; i<arr.size(); i++){
+		pages +=arr[i];
+		if(pages>maxPages){
+			count++;
+			pages = arr[i];
+		}
+	}
+	return count<=k;
+}
 
+int findPages(vector<int> &arr, int k) {
+    // Your code here
+	int n = arr.size();
+
+	if(k>n){
+		return -1;
+	}
+
+	int start=0, end = 0, ans;
+	for(int i=0;i<n;i++){
+		start = max(start,arr[i]);
+        end +=arr[i];
+	}
+
+
+
+	while(start<=end){
+		int mid = start +(end-start)/2;
+
+		if(allocateBook(mid, arr,k)){
+			ans = mid;
+			end = mid-1;
+		}else{
+			start= mid+1;
+		}
+	}
+
+	return ans;
+
+}
