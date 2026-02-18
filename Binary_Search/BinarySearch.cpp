fbcbaf8ca86e3d429719fa1292a2e5f213132a5c
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2026-02-18 15:09:03 +0530

    Add binary search implementation
    Implement binary search algorithm in BinarySearch.cpp

---
+++ Binary_Search/BinarySearch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>&arr, int target) {
+        // Write your code here
+
+		int start =0, end=arr.size()-1;
+		
+		while(start<=end){
+			int mid = start+(end-start)/2;
+
+			if(arr[mid]==target)
+			return mid;
+
+			else if(arr[mid]>target)
+			end = mid-1;
+			else
+			start=mid+1;
+		}
+		return -1;
+		
+    }
+};
